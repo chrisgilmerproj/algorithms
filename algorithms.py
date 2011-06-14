2bc0ee8b0c533562b5b1aaad8767065492c9d6ae
Chris Gilmer <chris.gilmer@gmail.com>
2011-06-14 07:28:11 -0700

    Added a timing algorithm

--- algorithms.py
+++ algorithms.py
@@ -1,15 +1,34 @@
 #!/usr/local/bin/python
 
+import time
+
+def print_timing(func):
+	""" A decorator to determine how long an algorithm takes to complete """
+	def wrapper(*arg,**kwargs):
+		t1 = time.clock()
+		res = func(*arg,**kwargs)
+		t2 = time.clock()
+		print '%s took %0.3fms' % (func.func_name, (t2-t1)*1000.0)
+		return res
+	return wrapper
+
+@print_timing
 def insertion_sort(A):
+	"""
+	Best:	 n
+	Average: n^2
+	Worst:   n^2
+	"""
 	for j in range(1,len(A)):
 		key = A[j]
 		i = j-1
-		print key, j, i
 		while i >= 0 and A[i] > key:
 			A[i+1] = A[i]
 			i = i - 1
 		A[i+1] = key
 	return A
+
+
 
 try:
 	from IPython.Shell import IPShellEmbed
