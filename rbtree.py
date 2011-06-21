03ddeab1f785d925fe0bab3b4d9f5ebc938d2eb2
Chris Gilmer <chris.gilmer@gmail.com>
2011-06-21 08:02:25 -0700

    Added node insertion

--- rbtree.py
+++ rbtree.py
@@ -27,3 +27,87 @@ class rbtree(object):
 	nil  = property(fget=lambda self: self._nil,  doc="The tree's nil node")
 	root = property(fget=lambda self: self._root, doc="The tree's root node")
 
+	def insert_key(self, key):
+		self.insert_node(rbnode(key=key))
+	
+	def insert_node(self, z):
+		y = self.nil
+		x = self.root
+		while x != self.nil:
+			y = x
+			if z.key < x.key:
+				x = x.left
+			else:
+				x = x.right
+		z._p = y
+		if y == self.nil:
+			self._root = z
+		elif z.key < y.key:
+			y._left = z
+		else:
+			y._right = z
+		z._left = self.nil
+		z._right = self.nil
+		z._red = True
+		self._insert_fixup(z)
+	
+	def _insert_fixup(z):
+		while z.p.red:
+			if z.p == z.p.p.left:
+				y = z.p.p.right
+				if y.red:
+					z.p._red = False
+					y._red = False
+					z.p.p._red = True
+					z = z.p.p
+				else:
+					if z == z.p.right:
+						z = z.p
+						self._left_rotate(z)
+			else:
+                y = z.p.p.left
+                if y.red:
+                    z.p._red = False
+                    y._red = False
+                    z.p.p._red = True
+                    z = z.p.p
+                else:
+                    if z == z.p.left:
+                        z = z.p
+                        self._right_rotate(z)
+                    z.p._red = False
+                    z.p.p._red = True
+                    self._left_rotate(z.p.p)
+        self.root._red = False
+
+	def _left_rotate(self,x):
+		y = x.right
+		x._right = y.left
+		if y.left != self.nil:
+			y.left._p = x
+		y._p = x.p
+		if x.p == self.nil:
+			self._root = y
+		elif x == x.p.left:
+			x.p._left = y
+		else:
+			x.p._right = y
+		y._left = x
+		x._p = y
+	
+	def _right_rotate(self,y):
+		x = y.left
+		y._left = x.right
+		if x.right != self.nil:
+			x.right._p = x
+		x._p = y.p
+		if y.p == self.nil:
+			self._root = x
+		elif y == y.p.right:
+			y.p._right = x
+		else:
+			y.p._left = x
+		x._right = y
+		y._p = x
+	
+	
