2b405a3ecca4c03891dbcf5ef6ddd843b072bc4a
Chris Gilmer <chris.gilmer@gmail.com>
2011-06-23 08:23:36 -0700

    Compliant with PEP8

--- algorithms.py
+++ algorithms.py
@@ -1,19 +1,23 @@
 #!/usr/local/bin/python
 
-import random, time
+import random
+import time
+
 
 def print_timing(func):
     """ A decorator to determine how long an algorithm takes to complete """
-    def wrapper(*arg,**kwargs):
+    def wrapper(*arg, **kwargs):
         t1 = time.clock()
-        res = func(*arg,**kwargs)
+        res = func(*arg, **kwargs)
         t2 = time.clock()
-        print '%s\ttook %0.3fms' % (func.func_name, (t2-t1)*1000.0)
+        print '%s\ttook %0.3fms' % (func.func_name, (t2 - t1) * 1000.0)
         return res
     return wrapper
 
+
 def gen_array(x):
-    return [random.randint(1,x) for i in xrange(0,x)]
+    return [random.randint(1, x) for i in xrange(0, x)]
+
 
 @print_timing
 def insertion_sort(A):
@@ -22,17 +26,18 @@ def insertion_sort(A):
     Average: n^2
     Worst:   n^2
     """
-    for j in range(1,len(A)):
+    for j in range(1, len(A)):
         key = A[j]
-        i = j-1
+        i = j - 1
         while i >= 0 and A[i] > key:
-            A[i+1] = A[i]
+            A[i + 1] = A[i]
             i = i - 1
-        A[i+1] = key
+        A[i + 1] = key
 
-def merge(left,right):
+
+def merge(left, right):
     result = []
-    i,j = 0,0
+    i, j = 0, 0
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
             result.append(left[i])
@@ -44,6 +49,7 @@ def merge(left,right):
     result += right[j:]
     return result
 
+
 def merge_sort(A):
     """
     Best:    nlogn
@@ -52,15 +58,17 @@ def merge_sort(A):
     """
     if len(A) < 2:
         return A
-    middle = len(A)/2
-    left   = merge_sort(A[:middle])
-    right  = merge_sort(A[middle:])
-    return merge(left,right)
+    middle = len(A) / 2
+    left = merge_sort(A[:middle])
+    right = merge_sort(A[middle:])
+    return merge(left, right)
+
 
 @print_timing
 def merge_time(A):
     """ Method for timing merge_sort """
     return merge_sort(A)
+
 
 @print_timing
 def bubble_sort(A):
@@ -69,15 +77,17 @@ def bubble_sort(A):
     Average: n^2
     Worst:   n^2
     """
-    for i in range(0,len(A)-1):
-        for j in range(len(A)-1,i,-1):
-            if A[j] < A[j-1]:
-                A[j], A[j-1] = A[j-1], A[j]
+    for i in range(0, len(A) - 1):
+        for j in range(len(A) - 1, i, -1):
+            if A[j] < A[j - 1]:
+                A[j], A[j - 1] = A[j - 1], A[j]
     return A
+
 
 @print_timing
 def quicksort_time(A):
     return quicksort(A)
+
 
 def quicksort(A):
     """
@@ -85,24 +95,28 @@ def quicksort(A):
     Average: nlogn
     Worst:   n^2
     """
-    if A == []: return A
+    if A == []:
+        return A
     q = A.pop(random.randrange(len(A)))
-    left  = quicksort([l for l in A if l < q])
+    left = quicksort([l for l in A if l < q])
     right = quicksort([l for l in A if l < q])
     return left + [q] + right
 
+
 @print_timing
 def selection_sort(A):
-   for x in range(0, len(A)-1):
-      q = x
-      for y in range(x+1, len(A)):
-         if A[y] < A[q]:
-            q = y
-      A[x], A[q] = A[q], A[x]
+    for x in range(0, len(A) - 1):
+        q = x
+        for y in range(x + 1, len(A)):
+            if A[y] < A[q]:
+                q = y
+        A[x], A[q] = A[q], A[x]
+
 
 @print_timing
 def python_sort(A):
     A.sort()
+
 
 def test(x):
     A = gen_array(x)
@@ -111,7 +125,7 @@ def test(x):
     A = gen_array(x)
     merge_time(A)
     bubble_sort(A)
-    
+
     A = gen_array(x)
     quicksort_time(A)
     selection_sort(A)
@@ -123,4 +137,3 @@ try:
 except:
     import code
     code.interact()
-
